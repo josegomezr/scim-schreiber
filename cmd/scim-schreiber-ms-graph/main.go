8cbcbb203cc696750619d67900a65dc7a8a3095e
Jose D. Gomez R. <jose.gomez@suse.com>
2026-03-23 18:29:07 +0100

    feat(msgraph): MS Graph backend
    Minimal implementation to interface against MS Graph API
    
    It fronts a SCIM interface to a very basic CRUD over the MS-Graph API.
    
    Get requests do not care about pagination, they dump the whole directory
    in one go.
    
    Updates are only supported in PUT (Replace ops)
    
    Deletions are done in stages (rename principal, rename unique id, then
    finally delete to avoid eventual-consistency locks)

---
+++ cmd/scim-schreiber-ms-graph/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/elimity-com/scim"
+	"github.com/elimity-com/scim/optional"
+	"github.com/josegomezr/scim-schreiber-ldap/internal/model"
+	"github.com/josegomezr/scim-schreiber-ldap/internal/msgraph"
+)
+
+type Config struct {
+	ClientId          string
+	ClientSecret      string
+	TenantId          string
+	TenantDomain      string
+	OnMicrosoftDomain string
+}
+
+func main() {
+	programLevel := new(slog.LevelVar)
+
+	if logLevel := os.Getenv("LOG_LEVEL"); logLevel != "" {
+		if err := programLevel.UnmarshalText([]byte(logLevel)); err != nil {
+			slog.Info("Falling back to LOG_LEVEL Info")
+		}
+	}
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: programLevel}))
+	slog.SetDefault(logger)
+
+	cfg := Config{
+		ClientId:          os.Getenv("AZURE_CLIENT_ID"),
+		ClientSecret:      os.Getenv("AZURE_CLIENT_SECRET"),
+		TenantId:          os.Getenv("AZURE_TENANT_ID"),
+		TenantDomain:      os.Getenv("AZURE_TENANT_DOMAIN"),
+		OnMicrosoftDomain: os.Getenv("AZURE_ONMICROSOFT_DOMAIN"),
+	}
+
+	server, err := createSCIMServer(cfg)
+
+	if err != nil {
+		slog.Error("Failed to create server", "err", err)
+		return
+	}
+
+	startHttpServer(server, err)
+}
+
+func startHttpServer(server scim.Server, err error) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /-/live", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	})
+
+	mux.Handle("/", server)
+
+	listenAddr := ":9440"
+	slog.Info("Listening", "listenAddr", listenAddr)
+	// TODO(josegomezr): configurable ports here
+	err = http.ListenAndServe(listenAddr, mux)
+
+	if err != nil {
+		slog.Error("Failed to start http server", "err", err)
+		return
+	}
+}
+
+func createSCIMServer(cfg Config) (scim.Server, error) {
+	msgraphClient, err := msgraph.NewClient(func(mscfg *msgraph.Config) {
+		mscfg.ClientId = cfg.ClientId
+		mscfg.ClientSecret = cfg.ClientSecret
+		mscfg.TenantId = cfg.TenantId
+		mscfg.TenantDomain = cfg.TenantDomain
+		mscfg.OnMicrosoftDomain = cfg.OnMicrosoftDomain
+	})
+
+	if err != nil {
+		return scim.Server{}, err
+	}
+
+	config := scim.ServiceProviderConfig{
+		AuthenticationSchemes: []scim.AuthenticationScheme{
+			{
+				Type: scim.AuthenticationTypeHTTPBasic,
+				Name: "HTTP Basic",
+			},
+		},
+		MaxResults:       100,
+		SupportFiltering: true,
+	}
+
+	resourceTypes := []scim.ResourceType{
+		{
+			ID:          optional.NewString("User"),
+			Name:        "User",
+			Endpoint:    "/Users",
+			Description: optional.NewString("User Account"),
+			Schema:      model.UserSchema,
+			Handler: UserHandler{
+				cfg:    &cfg,
+				client: msgraphClient,
+			},
+		},
+	}
+
+	serverArgs := &scim.ServerArgs{
+		ServiceProviderConfig: &config,
+		ResourceTypes:         resourceTypes,
+	}
+
+	serverOpts := []scim.ServerOption{
+		scim.WithLogger(&ScimLogger{}), // optional, default is no logging
+	}
+
+	return scim.NewServer(serverArgs, serverOpts...)
+}
---
+++ cmd/scim-schreiber-ms-graph/user_handler.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	// "io"
+	// "os"
+	// "encoding/json"
+	"net/http"
+
+	"github.com/elimity-com/scim"
+	"github.com/elimity-com/scim/errors"
+	"github.com/elimity-com/scim/filter"
+	"github.com/elimity-com/scim/optional"
+	"github.com/josegomezr/scim-schreiber-ldap/internal/msgraph"
+	scim_filter_parser "github.com/scim2/filter-parser/v2"
+)
+
+type UserHandler struct {
+	cfg    *Config
+	client *msgraph.Client
+}
+
+func (h UserHandler) Create(r *http.Request, attributes scim.ResourceAttributes) (scim.Resource, error) {
+	slog.Info("POST /v2/Users", "request", attributes)
+
+	userRequest := resourceToMsUser(attributes)
+	user, err := h.client.CreateUser(*userRequest)
+	if err != nil {
+		return scim.Resource{}, errors.ScimError{Status: http.StatusInternalServerError, Detail: fmt.Sprintf("%s", err)}
+	}
+
+	return msUserToUserResource(user), nil
+}
+
+func (h UserHandler) Delete(r *http.Request, id string) error {
+	slog.Info("DELETE /v2/Users", "id", id)
+	err := h.client.DeleteUser(id)
+	if err != nil {
+		return errors.ScimError{Status: http.StatusInternalServerError, Detail: fmt.Sprintf("%s", err)}
+	}
+
+	return nil
+}
+
+func (h UserHandler) Get(r *http.Request, id string) (scim.Resource, error) {
+	slog.Info("GET /v2/Users", "id", id)
+
+	if id == "" {
+		return scim.Resource{}, errors.ScimErrorResourceNotFound("")
+	}
+
+	msUser, err := h.client.GetUser(id)
+	if err != nil {
+		return scim.Resource{}, err
+	}
+
+	if msUser == nil {
+		return scim.Resource{}, errors.ScimErrorResourceNotFound(id)
+	}
+
+	return msUserToUserResource(msUser), nil
+}
+
+func principalFromFilter(filterValidator *filter.Validator) (string, error) {
+	if filterValidator == nil {
+		return "", nil
+	}
+	f, ok := filterValidator.GetFilter().(*scim_filter_parser.AttributeExpression)
+	if !ok {
+		return "", fmt.Errorf("only single expressions are supported")
+	}
+	if f.Operator != "eq" {
+		return "", fmt.Errorf("only operator 'eq' is supported in filters")
+	}
+	if f.AttributePath.AttributeName != "userName" {
+		return "", fmt.Errorf("only 'userName' is supported in filters")
+	}
+	return f.CompareValue.(string), nil
+}
+
+func msUserToUserResource(entry *msgraph.User) scim.Resource {
+	return scim.Resource{
+		ID:         entry.Id,
+		ExternalID: optional.NewString(entry.OnPremisesImmutableId),
+		Attributes: map[string]interface{}{
+			"givenName":         entry.GivenName,
+			"surname":           entry.Surname,
+			"userName":          entry.MailNickname,
+			"displayName":       entry.DisplayName,
+			"mailNickname":      entry.MailNickname,
+			"userPrincipalName": entry.UserPrincipalName,
+			"accountEnabled":    entry.AccountEnabled,
+		},
+	}
+}
+
+func CastSingleValue[T any](input interface{}) T {
+	output, _ := input.(T)
+	return output
+}
+
+func resourceToMsUser(resourceAttrs map[string]interface{}) *msgraph.User {
+	nameMap := CastSingleValue[map[string]interface{}](resourceAttrs["name"])
+
+	return &msgraph.User{
+		GivenName:             CastSingleValue[string](nameMap["givenName"]),
+		Surname:               CastSingleValue[string](nameMap["familyName"]),
+		DisplayName:           CastSingleValue[string](resourceAttrs["displayName"]),
+		MailNickname:          CastSingleValue[string](resourceAttrs["userName"]),
+		UserPrincipalName:     CastSingleValue[string](resourceAttrs["userName"]),
+		AccountEnabled:        CastSingleValue[bool](resourceAttrs["active"]),
+		OnPremisesImmutableId: CastSingleValue[string](resourceAttrs["externalId"]),
+	}
+}
+
+func (h UserHandler) GetAll(r *http.Request, params scim.ListRequestParams) (scim.Page, error) {
+	slog.Info("GET /v2/Users", "params", params)
+	principal, err := principalFromFilter(params.FilterValidator)
+	if err != nil {
+		return scim.Page{}, err
+	}
+
+	resources := make([]scim.Resource, 0)
+	for msUser, err := range h.client.ListAllUsers(principal) {
+		if err != nil {
+			return scim.Page{}, err
+		}
+		resources = append(resources, msUserToUserResource(msUser))
+	}
+
+	return scim.Page{
+		TotalResults: len(resources),
+		Resources:    resources,
+	}, nil
+}
+
+func (h UserHandler) Patch(r *http.Request, id string, operations []scim.PatchOperation) (scim.Resource, error) {
+	slog.Info("PATCH /v2/Users", "id", id, "operations", operations)
+	return scim.Resource{}, errors.ScimError{Status: http.StatusNotImplemented, Detail: "Patch is not implemented for users"}
+}
+
+func (h UserHandler) Replace(r *http.Request, id string, attributes scim.ResourceAttributes) (scim.Resource, error) {
+	slog.Info("PUT /v2/Users", "id", id, "attributes", attributes)
+
+	userRequest := resourceToMsUser(attributes)
+	msUser, err := h.client.UpdateUser(id, *userRequest)
+	if err != nil {
+		return scim.Resource{}, errors.ScimError{Status: http.StatusInternalServerError, Detail: fmt.Sprintf("%s", err)}
+	}
+	return msUserToUserResource(msUser), nil
+}
