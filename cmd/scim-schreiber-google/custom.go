07ca8b92e6952bee6702f043f61ee78d9195e918
Felix Prasse <felix.prasse@suse.com>
2026-07-28 17:41:27 +0200

    Add custom schema management

--- cmd/scim-schreiber-google/custom.go
+++ cmd/scim-schreiber-google/custom.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"log/slog"
 	"reflect"
+	"slices"
 	"strings"
 
+	admin "google.golang.org/api/admin/directory/v1"
 	"google.golang.org/api/googleapi"
 )
 
@@ -108,4 +111,227 @@ func (c CustomFields) MarshalGoogleApi() (map[string]googleapi.RawMessage, error
 	}
 
 	return ret, nil
+}
+
+type CustomField struct {
+	Name        string
+	DisplayName string
+	MultiValued bool
+	FieldType   string
+}
+
+type CustomSchema struct {
+	Name        string
+	DisplayName string
+	Fields      []CustomField
+}
+
+// This is a complete legacy spec created by GWS application owners.
+// Which is why each field has it's own schema instead of one schema with multiple fields.
+func CustomSchemaSpec() []CustomSchema {
+	return []CustomSchema{
+		{
+			Name:        "Region7281",
+			DisplayName: "Region",
+			Fields: []CustomField{
+
+				{
+					Name:        "Office",
+					DisplayName: "Office",
+					MultiValued: false,
+					FieldType:   "STRING",
+				},
+			},
+		},
+
+		{
+			Name:        "User1921",
+			DisplayName: "User",
+			Fields: []CustomField{
+
+				{
+					Name:        "isSupervisor",
+					DisplayName: "isSupervisor",
+					MultiValued: false,
+					FieldType:   "BOOL",
+				},
+			},
+		},
+
+		{
+			Name:        "ProxyAddresses",
+			DisplayName: "ProxyAddresses",
+			Fields: []CustomField{
+
+				{
+					Name:        "ProxyAddresses",
+					DisplayName: "ProxyAddresses",
+					MultiValued: true,
+					FieldType:   "STRING",
+				},
+			},
+		},
+
+		{
+			Name:        "Region",
+			DisplayName: "Region",
+			Fields: []CustomField{
+
+				{
+					Name:        "addressesWorkCountryCode",
+					DisplayName: "addressesWorkCountryCode",
+					MultiValued: false,
+					FieldType:   "STRING",
+				},
+			},
+		},
+
+		{
+			Name:        "Senior_Leader",
+			DisplayName: "Senior Leader",
+			Fields: []CustomField{
+
+				{
+					Name:        "senior_leaders",
+					DisplayName: "secondaryWorkforceManagerID",
+					MultiValued: false,
+					FieldType:   "STRING",
+				},
+			},
+		},
+
+		{
+			Name:        "User",
+			DisplayName: "User",
+			Fields: []CustomField{
+
+				{
+					Name:        "userType",
+					DisplayName: "userType",
+					MultiValued: false,
+					FieldType:   "STRING",
+				},
+			},
+		},
+
+		{
+			Name:        "User2471",
+			DisplayName: "User",
+			Fields: []CustomField{
+
+				{
+					Name:        "workLocationType",
+					DisplayName: "workLocationType",
+					MultiValued: false,
+					FieldType:   "STRING",
+				},
+			},
+		},
+
+		{
+			Name:        "Status",
+			DisplayName: "Status",
+			Fields: []CustomField{
+
+				{
+					Name:        "User_status",
+					DisplayName: "User status",
+					MultiValued: false,
+					FieldType:   "STRING",
+				},
+			},
+		},
+
+		{
+			Name:        "Job_Family",
+			DisplayName: "Job Family",
+			Fields: []CustomField{
+
+				{
+					Name:        "jobfamily5241",
+					DisplayName: "jobfamily",
+					MultiValued: false,
+					FieldType:   "STRING",
+				},
+			},
+		},
+
+		{
+			Name:        "L3_Leader",
+			DisplayName: "L3 Leader",
+			Fields: []CustomField{
+
+				{
+					Name:        "L3_Leader",
+					DisplayName: "userLeader",
+					MultiValued: false,
+					FieldType:   "STRING",
+				},
+			},
+		},
+
+		{
+			Name:        "User7951",
+			DisplayName: "User",
+			Fields: []CustomField{
+
+				{
+					Name:        "division",
+					DisplayName: "division",
+					MultiValued: false,
+					FieldType:   "STRING",
+				},
+			},
+		},
+	}
+}
+
+func SyncSchemas(adminClient *admin.Service) error {
+	schemas, err := adminClient.Schemas.List("my_customer").Do()
+
+	if err != nil {
+		return err
+	}
+
+	customSchemas := CustomSchemaSpec()
+
+	for _, customSchema := range customSchemas {
+		slog.Info("Syncing schema", "schema", customSchema.Name)
+
+		fields := make([]*admin.SchemaFieldSpec, 0, len(customSchema.Fields))
+
+		for _, field := range customSchema.Fields {
+			fields = append(fields, &admin.SchemaFieldSpec{
+				DisplayName:    field.DisplayName,
+				FieldName:      field.Name,
+				FieldType:      field.FieldType,
+				MultiValued:    field.MultiValued,
+				ReadAccessType: "ADMINS_AND_SELF",
+			})
+		}
+
+		newSchema := admin.Schema{
+			DisplayName: customSchema.DisplayName,
+			SchemaName:  customSchema.Name,
+			Fields:      fields,
+		}
+
+		schemaExists := slices.ContainsFunc(schemas.Schemas, func(s *admin.Schema) bool {
+			return strings.EqualFold(s.SchemaName, customSchema.Name)
+		})
+
+		if schemaExists {
+			_, err := adminClient.Schemas.Update("my_customer", customSchema.Name, &newSchema).Do()
+			if err != nil {
+				return err
+			}
+		} else {
+			_, err := adminClient.Schemas.Insert("my_customer", &newSchema).Do()
+
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
